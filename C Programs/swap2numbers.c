73df41259de3375760c7975423bc8778d44f4cde
anujajoshi0503 <72315239+anujajoshi0503@users.noreply.github.com>
2020-10-27 16:05:09 +0530

    Update swap2numbers.c

--- C Programs/swap2numbers.c
+++ C Programs/swap2numbers.c
@@ -1,13 +1,16 @@
 #include<stdio.h>  
 int main()    
 {    
-int a,b;
+int a,b,c=0;
 printf("Enter the 2 numbers to be swapped\n");
 scanf("%d%d",&a,&b);      
 printf("Before swap a=%d b=%d",a,b);      
-a=a+b; 
-b=a-b;    
-a=a-b;   
+c = a;
+a = b;
+b = c;
+  //a=a+b; 
+//b=a-b;    
+//a=a-b;   
 printf("\nAfter swap a=%d b=%d",a,b);    
 return 0;  
 }  
